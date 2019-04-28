cf61c0de1b213607b7806c3abc47f03bcb1b9ff9
Matheus <matheusangelc@outlook.com>
2019-04-28 07:40:47 -0300

    Adicionar arquivos de projeto.

---
+++ Hackathon/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Hackathon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hackathon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.VagasNaoOcupadas = 50;
+            ViewBag.VagasOcupadas = 75;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
